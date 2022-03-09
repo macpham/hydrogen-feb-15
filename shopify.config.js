ae0445ae2c2e78ec9f70693085f2f83c733f6b5e
Mac Pham <1777724+macpham@users.noreply.github.com>
2022-03-09 14:39:53 -0500

    Update shopify.config.js

--- shopify.config.js
+++ shopify.config.js
@@ -1,6 +1,6 @@
 export default {
   defaultLocale: 'en',
   storeDomain: 'macs-dev-store.myshopify.com',
-  storefrontToken: '4cfdffd7fc5ac8de65abf08d64a55233',
+  storefrontToken: 'thisiswrong',
   storefrontApiVersion: 'unstable',
 };
